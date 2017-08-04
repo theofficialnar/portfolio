dbb02d0dd0963417b3baacfe7b1483a493f85ddd
Nar Cuenca <narc.ph@gmail.com>
2017-08-04 16:31:29 +0800

    moved files

--- partials/contact.php
+++
@@ -1,4 +0,0 @@
-<div id="contact" class="tab-pane fade">
-	<h1 class="heading-no-margin">Contact Page</h1>
-	<p>Sample content</p>
-</div>
--- partials/work.php
+++
@@ -1,4 +0,0 @@
-<div id="work" class="tab-pane fade">
-	<h1 class="heading-no-margin">Work Page</h1>
-	<p>Sample content</p>
-</div>
