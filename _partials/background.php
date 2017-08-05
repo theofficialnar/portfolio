e7277d62662a017dc9dd8ae4097aa5254af09f8b
Nar Cuenca <narc.ph@gmail.com>
2017-08-05 14:55:11 +0800

    updated about me style

--- _partials/background.php
+++ _partials/background.php
@@ -6,6 +6,7 @@
 			<hr>
 		</div>
 	</div>
+	<!-- Educational Background -->
 	<div class="row">
 		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2">
 			<h3>Educational Background</h3>
@@ -13,30 +14,50 @@
 				<!-- FOR DESKTOP AND TABLET VIEW -->
 				<div class="col-lg-5 col-md-5 col-sm-5 hidden-xs">
 					<div class="bubble-left">
-						<span style="color: black">Elementary<br>
-						San Vicente Alto Elementary School
-						</span>
+						<div class="text-container">
+							<h4 class="bubble-text">Coding Bootcamp</h4>
+							<hr style="margin: 0; border: 1px solid #393e41">
+							<span class="bubble-text">Tuitt Philippines</span>
+						</div>
 					</div>
 				</div>
 				<div class="col-lg-5 col-lg-offset-7 col-md-5 col-md-offset-7 col-sm-5 col-sm-offset-7 hidden-xs ">
-					<div class="bubble-right"></div>
+					<div class="bubble-right">
+						<div class="text-container">
+							<h4 class="bubble-text">College</h4>
+							<hr style="margin: 0; border: 1px solid #393e41">
+							<span class="bubble-text">Liceo de Cagayan University</span>
+						</div>
+					</div>
 				</div>
 				<div class="col-lg-5 col-md-5 col-sm-5 hidden-xs">
 					<div class="bubble-left">
-						<span style="color: black">Elementary<br>
-						San Vicente Alto Elementary School
-						</span>
+						<div class="text-container">
+							<h4 class="bubble-text">High School</h4>
+							<hr style="margin: 0; border: 1px solid #393e41">
+							<span class="bubble-text">Misamis Occidental National High School</span>
+						</div>
 					</div>
 				</div>
 				<div class="col-lg-5 col-lg-offset-7 col-md-5 col-md-offset-7 col-sm-5 col-sm-offset-7 hidden-xs ">
-					<div class="bubble-right"></div>
+					<div class="bubble-right">
+						<div class="text-container">
+							<h4 class="bubble-text">Elementary</h4>
+							<hr style="margin: 0; border: 1px solid #393e41">
+							<span class="bubble-text">San Vicente Alto Elementary School</span>
+						</div>
+					</div>
 				</div>
 				<!-- FOR MOBILE VIEW -->
 				<div class="col-xs-8 col-xs-offset-2">
 					<div class="panel-group hidden-sm hidden-md hidden-lg">
 						<div class="panel panel-default">
-							<div class="panel-heading"><strong>Elementary</strong></div>
-							<div class="panel-body" style="color: #393e41">San Vicente Alto Elementary School</div>
+							<div class="panel-heading"><strong>Coding Bootcamp</strong></div>
+							<div class="panel-body" style="color: #393e41">Tuitt Philippines</div>
+						</div>
+						<div class="panel panel-default">
+							<div class="panel-heading"><strong>College</strong></div>
+							<div class="panel-body" style="color: #393e41">Liceo de Cagayan University</div>
 						</div>
 						<div class="panel panel-default">
 							<div class="panel-heading"><strong>High School</strong></div>
@@ -44,20 +65,21 @@
 						</div>
 						<div class="panel panel-default">
 							<div class="panel-heading"><strong>Elementary</strong></div>
-							<div class="panel-body" style="color: #393e41">Liceo de Cagayan University</div>
+							<div class="panel-body" style="color: #393e41">San Vicente Alto Elementary School</div>
 						</div>
 					</div>
 				</div>
 				<!-- Yellow line -->
 				<div id="line" class="col-lg-6 col-md-6 col-sm-6 hidden-xs">
-					<div class="year1 hidden-xs">Year</div>
-					<div class="year2 hidden-xs">Year</div>
-					<div class="year3 hidden-xs">Year</div>
-					<div class="year4 hidden-xs">Year</div>
+					<div class="year1 hidden-xs">2017</div>
+					<div class="year2 hidden-xs">2012</div>
+					<div class="year3 hidden-xs">2006</div>
+					<div class="year4 hidden-xs">2002</div>
 				</div>
 			</div>
 		</div>
 	</div>
+	<!-- Work Experience -->
 	<div class="row margin-top">
 		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-xs-8 col-xs-offset-2">
 			<h3>Work Experience</h3>
@@ -110,153 +132,190 @@
 					<div id="collapse4" class="panel-collapse collapse">
 						<div class="panel-body">
 							<p class="panel-text panel-company">Really Social Media</p>
-							<p class="panel-text">March 2016 – May 2017</p>
+							<p class="panel-text">March 2016 – April 2017</p>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+	<!-- Skills -->
 	<div class="row">
 		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-xs-8 col-xs-offset-2">
 			<h3>Skills</h3>
+			<!-- html -->
 			<div class="row">
 				<div class="col-lg-3 col-lg-offset-3 col-md-6 col-sm-6">
 					<h3>HTML</h3>
 				</div>
 				<div class="col-lg-3 col-md-6 col-sm-6">
 					<h3>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
+						<div class="neon-bar">
+							<div class="neon-bg">
+								<div class="neon-progress html"></div>
+								<div class="neon-text">100%</div>
+							</div>
+						</div>
 					</h3>
 				</div>
 			</div>
+			<!-- css -->
 			<div class="row">
 				<div class="col-lg-3 col-lg-offset-3 col-md-6 col-sm-6">
 					<h3>CSS</h3>
 				</div>
 				<div class="col-lg-3 col-md-6 col-sm-6">
 					<h3>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
+						<div class="neon-bar">
+							<div class="neon-bg">
+								<div class="neon-progress css"></div>
+								<div class="neon-text">95%</div>
+							</div>
+						</div>
 					</h3>
 				</div>
 			</div>
+			<!-- Javascript -->
 			<div class="row">
 				<div class="col-lg-3 col-lg-offset-3 col-md-6 col-sm-6">
 					<h3>Javascript</h3>
 				</div>
 				<div class="col-lg-3 col-md-6 col-sm-6">
 					<h3>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
+						<div class="neon-bar">
+							<div class="neon-bg">
+								<div class="neon-progress jscript"></div>
+								<div class="neon-text">83%</div>
+							</div>
+						</div>
 					</h3>
 				</div>
 			</div>
+			<!-- jquery -->
 			<div class="row">
 				<div class="col-lg-3 col-lg-offset-3 col-md-6 col-sm-6">
 					<h3>JQuery</h3>
 				</div>
 				<div class="col-lg-3 col-md-6 col-sm-6">
 					<h3>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
+						<div class="neon-bar">
+							<div class="neon-bg">
+								<div class="neon-progress jquery"></div>
+								<div class="neon-text">83%</div>
+							</div>
+						</div>
 					</h3>
 				</div>
 			</div>
+			<!-- ajax -->
 			<div class="row">
 				<div class="col-lg-3 col-lg-offset-3 col-md-6 col-sm-6">
 					<h3>AJAX</h3>
 				</div>
 				<div class="col-lg-3 col-md-6 col-sm-6">
 					<h3>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
+						<div class="neon-bar">
+							<div class="neon-bg">
+								<div class="neon-progress ajax"></div>
+								<div class="neon-text">79%</div>
+							</div>
+						</div>
 					</h3>
 				</div>
 			</div>
+			<!-- php -->
 			<div class="row">
 				<div class="col-lg-3 col-lg-offset-3 col-md-6 col-sm-6">
 					<h3>PHP</h3>
 				</div>
 				<div class="col-lg-3 col-md-6 col-sm-6">
 					<h3>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
+						<div class="neon-bar">
+							<div class="neon-bg">
+								<div class="neon-progress php"></div>
+								<div class="neon-text">75%</div>
+							</div>
+						</div>
 					</h3>
 				</div>
 			</div>
+			<!-- laravel -->
+			<div class="row">
+				<div class="col-lg-3 col-lg-offset-3 col-md-6 col-sm-6">
+					<h3>Laravel</h3>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<h3>
+						<div class="neon-bar">
+							<div class="neon-bg">
+								<div class="neon-progress laravel"></div>
+								<div class="neon-text">60%</div>
+							</div>
+						</div>
+					</h3>
+				</div>
+			</div>
+			<!-- mysql -->
 			<div class="row">
 				<div class="col-lg-3 col-lg-offset-3 col-md-6 col-sm-6">
 					<h3>MySQL</h3>
 				</div>
 				<div class="col-lg-3 col-md-6 col-sm-6">
 					<h3>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
+						<div class="neon-bar">
+							<div class="neon-bg">
+								<div class="neon-progress mysql"></div>
+								<div class="neon-text">80%</div>
+							</div>
+						</div>
 					</h3>
 				</div>
 			</div>
+			<!-- photoshop -->
 			<div class="row">
 				<div class="col-lg-3 col-lg-offset-3 col-md-6 col-sm-6">
-					<h3>Photoshop</h3>
+					<h3>Photoshop / GIMP</h3>
 				</div>
 				<div class="col-lg-3 col-md-6 col-sm-6">
 					<h3>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
+						<div class="neon-bar">
+							<div class="neon-bg">
+								<div class="neon-progress pshop"></div>
+								<div class="neon-text">75%</div>
+							</div>
+						</div>
 					</h3>
 				</div>
 			</div>
+			<!-- sony vegas -->
 			<div class="row">
 				<div class="col-lg-3 col-lg-offset-3 col-md-6 col-sm-6">
 					<h3>Sony Vegas Pro</h3>
 				</div>
 				<div class="col-lg-3 col-md-6 col-sm-6">
 					<h3>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
+						<div class="neon-bar">
+							<div class="neon-bg">
+								<div class="neon-progress svegas"></div>
+								<div class="neon-text">79%</div>
+							</div>
+						</div>
 					</h3>
 				</div>
 			</div>
+			<!-- linux -->
 			<div class="row">
 				<div class="col-lg-3 col-lg-offset-3 col-md-6 col-sm-6">
 					<h3>Linux</h3>
 				</div>
 				<div class="col-lg-3 col-md-6 col-sm-6">
 					<h3>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
+						<div class="neon-bar">
+							<div class="neon-bg">
+								<div class="neon-progress linux"></div>
+								<div class="neon-text">80%</div>
+							</div>
+						</div>
 					</h3>
 				</div>
 			</div>
